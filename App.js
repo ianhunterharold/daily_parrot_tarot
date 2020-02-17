12053ec039a31e549d865c6271bcbc1b7951079b
ianhunterharold <ianhunterharold@gmail.com>
2020-02-17 17:48:06 -0500

    abstracted style to seperate folder and file

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar } from 'react-native';
 import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import Styles from './src/style/style.js'
 
 const App: () => React$Node = () => {
   return (
     <>
     
       <SafeAreaView>
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>   
+          <View style={Styles.body}>
+            <View style={Styles.sectionContainer}>   
             <Text>Hello World</Text>         
             </View>
           </View>
@@ -18,16 +19,16 @@ const App: () => React$Node = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  // will abstract out styling into seperate component and then import it
-  //task for monday feb 17th 2020
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  }
-});
+// const styles = StyleSheet.create({
+//   // will abstract out styling into seperate component and then import it
+//   //task for monday feb 17th 2020
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24
+//   }
+// });
 
 export default App;
---
+++ src/style/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+
+const Styles = StyleSheet.create({
+  // will abstract out styling into seperate component and then import it
+  //task for monday feb 17th 2020
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24
+  }
+});
+
+export default Styles;
