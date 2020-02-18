49a6fbd46bebc0e3b924e6e797e42df50c59e97e
ianhunterharold <ianhunterharold@gmail.com>
2020-02-18 16:33:25 -0500

    added skeleton button, starting build out of landing page

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Component } from 'react';
-import { View,Text } from 'react-native';
+import { View,Text,Button } from 'react-native';
+import Styles from '../style/style';
 
 class LandingPage extends Component{
 
   render(){
     return(
       <View>
+        <Button 
+        // style = { Styles.landingPageButton }
+        // looking at add border, not visible currently
+
+        title="venture"
+        border='visible'
+        >
+        </Button>
         <Text>In the landing page container</Text>
       </View>
     )
