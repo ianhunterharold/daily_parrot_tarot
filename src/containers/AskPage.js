2507519b70eb927c45774a161fc82e2d6d97496e
ianhunterharold <ianhunterharold@gmail.com>
2020-02-27 16:50:38 -0500

    added text input field

--- src/containers/AskPage.js
+++ src/containers/AskPage.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 class AskPage extends Component{
 
+  onChangeText = (Text) => {
+
+  }
 
   render(){
     return(
       <View>
         <Text>Inside of AskPage Container</Text>
+        <TextInput
+          style={{ height: 50, borderColor: 'gray', borderWidth: 2 }}
+          onChangeText={text => this.onChangeText(text)}
+          // value={value}
+        />
       </View>
     )
   }
