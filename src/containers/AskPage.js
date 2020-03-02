34e969f1ca9b9d6529f4d41af574a1322af25e93
ianhunterharold <ianhunterharold@gmail.com>
2020-03-02 16:09:14 -0500

    added card component when ask is pushed, still not showing exactly how I want it, baby steps

--- src/containers/AskPage.js
+++ src/containers/AskPage.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import { Component } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import Card from '../components/Card';
 
 class AskPage extends Component{
   state ={
-    question:''
+    question:'',
+    clickedOnAsk: 'false'
   }
 
   onChangeText = (Text) => {
-    console.log('Text that you input', Text)
+    // console.log('Text that you input', this.state.question)
     this.setState({
       question: Text
     })
   }
-  onSubmitQuestion = (input) =>{
+
+  renderCardComponent = () => {
+    if(this.state.clickedOnAsk === 'true'){
+      console.log(this.state.clickedOnAsk,"clicked on ask")
+      return <Card/>
+    }
+  }
+  onSubmitQuestion = (input) => {
     // console.log('input, did you clikc the submit', input) worked, great
     // alert("did i touch the button?")
     // remove all elements on the page 
+    this.setState({
+      clickedOnAsk: 'true'
+    }) 
   }
-
+    
 
   render(){
     return(
       <View>
+        {this.renderCardComponent()}
         <Text>Inside of AskPage Container</Text>
         <TextInput
           style={{ height: 50, borderColor: 'gray', borderWidth: 2 }}
