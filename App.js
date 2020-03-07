f752a7aec89251882d08af3e7649549bab00848a
ianhunterharold <ianhunterharold@gmail.com>
2020-03-07 12:23:39 -0500

    revamped landing page

--- App.js
+++ App.js
@@ -5,44 +5,25 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'
 
-// import React from 'react';
-// import { Component } from 'react';
-// import { SafeAreaView, StyleSheet, ScrollView, View, Text, Button, StatusBar } from 'react-native';
-
-// import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
-
 import Styles from './src/style/style.js'
 import AskPage from './src/containers/AskPage'
 import LandingPage from './src/containers/LandingPage';
 import Card from './src/components/Card';
-// import AppNavigator from './src/containers/AppNavigator';
 
-
-// class App extends Component {
-  
-//   render(){
-//     return(
-//       // <>
-//       //   <View style={Styles.LandingPageContainer}>
-//       //   <Text>inside of app</Text>
-//           {/* <LandingPage/> */}
-//           <NavigationContainer>{ <AppNavigator/> }</NavigationContainer>
-//         {/* </View>
-//       </> */}
-//     )
-//   }
-// }
-
-// export default App;
-// {/* <AppNavigator/> */}
+// import React from 'react';
+// import { Component } from 'react';
+// import { SafeAreaView, StyleSheet, ScrollView, View, Text, Button, StatusBar } from 'react-native';
+// import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator  initialRouteName="LandingPage">
         <Stack.Screen name="LandingPage" component={LandingPage} />
+        <Stack.Screen name="AskPage" component={AskPage}/>
+        <Stack.Screen name="Card" component={Card}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import { Component } from 'react';
-import { View,Text,Button } from 'react-native';
-import Styles from '../style/style';
-import AskPage from './AskPage';
+import { Button, View, Text } from 'react-native';
 
-
-class LandingPage extends Component{
-  state = {
-    askPageComponentRender: 'false',
-  }
-
-  handleLandingPageTouch = () => {
-    //after touching the button render, a new component
-    this.setState({
-      askPageComponentRender: 'true'
-    })
-  }
-
-  renderAskPageComponent = () => {
-    if (this.state.askPageComponentRender === 'true'){
-      return <AskPage/>
-    }
-  }
-
-  render(){
-    return(
-      <View >
-        {this.renderAskPageComponent()}
-        <Button 
-          onPress={this.handleLandingPageTouch}
-          title="venture"
-        />
-      </View>
-    )
-  }
+export default function LandingPage({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>
+        picture and better button
+      </Text>
+      <Button 
+        title="Venture"
+        onPress={() => navigation.navigate('AskPage')}
+      />
+    </View>
+  );
 }
-
-export default LandingPage;
