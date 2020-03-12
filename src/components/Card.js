bec3462c8e488c7bf70947b18e0fd1b3081a3e85
ianhunterharold <ianhunterharold@gmail.com>
2020-03-12 14:39:08 -0400

    assigned consts to three images, place in an array and selecting a random element from the array. returning type number with an actual number.

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,24 +7,25 @@ import cuckoo from '../img/cuckoo.png';
 import peacock from '../img/peacock.png';
 import plover from '../img/plover.png';
 
+const CUCKOO= cuckoo;
+console.log(CUCKOO)
+const PEACOCK= peacock;
+const PLOVER= plover;
 
 class Card extends Component{
   state = {
-    arrayOfBirds: [cuckoo, peacock, plover]
+    randomBird:''
   }
 
 
+
   randomlySelectABird = () => {
-    let randomBird = this.state.arrayOfBirds[2]
-    console.log("randombird", randomBird)  
-    console.log("are we inside of random bird")
-    //at this point prove that it can work before randomizing it. 
-    // return (
-    //   <Image 
-    //     source={require(`../img${randomBird}.png`)}
-    //     style={Styles.cardImageStyling}
-    //   />
-    // )
+    let randomBirdsArray = [ CUCKOO, PLOVER, PEACOCK ]
+    // randomly selects a bird from the array 
+    let randomSelectionOfBird = randomBirdsArray[Math.floor(Math.random() * randomBirdsArray.length)] 
+    console.log(typeof randomSelectionOfBird)
+    // display bird on the page 
+    // reference to an image is giving me a number, with type of number. 
   }
 
   render(){
@@ -33,7 +34,7 @@ class Card extends Component{
       <View>
         {this.randomlySelectABird()}
         <Image
-          source={require('../img/peacock.png')}
+          source={CUCKOO}
           style={Styles.cardImageStyling}
         />
         <Text style ={Styles.cardDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
