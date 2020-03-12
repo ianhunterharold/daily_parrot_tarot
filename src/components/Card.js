26ee38dff9ebe166f8c6aad18a5f77aa8e747d89
ianhunterharold <ianhunterharold@gmail.com>
2020-03-12 14:52:42 -0400

    getting closer to truly randomizing component with image and description.

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,10 +7,6 @@ import cuckoo from '../img/cuckoo.png';
 import peacock from '../img/peacock.png';
 import plover from '../img/plover.png';
 
-const CUCKOO= cuckoo;
-console.log(CUCKOO)
-const PEACOCK= peacock;
-const PLOVER= plover;
 
 class Card extends Component{
   state = {
@@ -18,14 +14,32 @@ class Card extends Component{
   }
 
 
-
   randomlySelectABird = () => {
+
+              const CUCKOO= <Image
+                source={cuckoo}
+                style={Styles.cardImageStyling}
+              />
+
+                const PEACOCK= <Image
+                  source={peacock}
+                  style={Styles.cardImageStyling}
+                /> 
+
+              const PLOVER = <Image
+                source={peacock}
+                style={Styles.cardImageStyling}
+              /> 
+
     let randomBirdsArray = [ CUCKOO, PLOVER, PEACOCK ]
     // randomly selects a bird from the array 
     let randomSelectionOfBird = randomBirdsArray[Math.floor(Math.random() * randomBirdsArray.length)] 
     console.log(typeof randomSelectionOfBird)
     // display bird on the page 
     // reference to an image is giving me a number, with type of number. 
+    // assign the constant to be a react native image component 
+    console.log(randomSelectionOfBird)
+    return randomSelectionOfBird
   }
 
   render(){
@@ -33,11 +47,7 @@ class Card extends Component{
       <>
       <View>
         {this.randomlySelectABird()}
-        <Image
-          source={CUCKOO}
-          style={Styles.cardImageStyling}
-        />
-        <Text style ={Styles.cardDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+        <Text style ={Styles.cardDescription}>11111 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
       </View>
       </>
     )
