c939b8b603dfb00b7bd38d1348743c8ccd45f382
ianhunterharold <ianhunterharold@gmail.com>
2020-03-13 15:46:52 -0400

    adding more birds and their images to the img file to render correctly.

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,48 +6,50 @@ import Styles from '../style/style';
 import cuckoo from '../img/cuckoo.png';
 import peacock from '../img/peacock.png';
 import plover from '../img/plover.png';
-
+import ostrich from '../img/ostrich.png'
+import flamingo from '../img/flamingo.png';
+import turquoiseParrot from '../img/turquoiseParrot.png'
 
 class Card extends Component{
-  state = {
-    randomBird:''
-  }
-
 
   randomlySelectABird = () => {
 
-              const CUCKOO= <Image
-                source={cuckoo}
+              const CUCKOO= <><Image
+                source={flamingo}
                 style={Styles.cardImageStyling}
-              />
-
-                const PEACOCK= <Image
-                  source={peacock}
+              /><Text style ={Styles.cardDescription}>11111111 cuckoo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text></>
+              
+                const PEACOCK= <><Image
+                  source={turquoiseParrot}
                   style={Styles.cardImageStyling}
-                /> 
+                /><Text style ={Styles.cardDescription}>22222 cuckoo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text></>
 
-              const PLOVER = <Image
-                source={peacock}
+              const PLOVER =<><Image
+                source={flamingo} // just sourcing the ostrich to see what it looks like neck is cut off 
                 style={Styles.cardImageStyling}
-              /> 
+              /><Text style ={Styles.cardDescription}>33333 cuckoo Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text></>
 
     let randomBirdsArray = [ CUCKOO, PLOVER, PEACOCK ]
     // randomly selects a bird from the array 
     let randomSelectionOfBird = randomBirdsArray[Math.floor(Math.random() * randomBirdsArray.length)] 
-    console.log(typeof randomSelectionOfBird)
-    // display bird on the page 
-    // reference to an image is giving me a number, with type of number. 
-    // assign the constant to be a react native image component 
+
+    this.accompanyTextWithCorespondingBird(randomSelectionOfBird)
     console.log(randomSelectionOfBird)
     return randomSelectionOfBird
+  }
+
+  accompanyTextWithCorespondingBird = (randomSelectionOfBird) => {
+    console.log(randomSelectionOfBird,"can we see the randomSelectionOfBird in here?")
+    
+    // match some identifier on the object and return the text
+
   }
 
   render(){
     return(
       <>
       <View>
-        {this.randomlySelectABird()}
-        <Text style ={Styles.cardDescription}>11111 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+      {this.randomlySelectABird()}
       </View>
       </>
     )
